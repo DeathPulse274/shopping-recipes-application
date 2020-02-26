e0e8e304ae5c5533ed085debd4799b68a94ce28d
santhosh <santhosh.k@rapyder.com>
2020-02-26 18:54:35 +0530

    added the modal for the recipe structure

---
+++ src/app/recipes/recipie.modal.ts
@@ -0,0 +1,11 @@
+export class recipe{
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, description: string , imagepath: string ){
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagepath;
+    }
+}
